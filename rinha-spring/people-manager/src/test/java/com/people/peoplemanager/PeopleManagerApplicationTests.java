1f6088c001c2f10b7e2388e9f1a089406abc6a15
João Victor <joaovictorf01@gmail.com>
2023-08-02 05:45:27 -0300

    Setup inicial do projeto

---
+++ rinha-spring/people-manager/src/test/java/com/people/peoplemanager/PeopleManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.people.peoplemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeopleManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
