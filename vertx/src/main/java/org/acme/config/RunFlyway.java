17219db151ee9e2dc3b9002908fd2a79e04babaa
André Spironello Tibola <andre.spironello12@gmail.com>
2023-08-05 09:25:29 -0300

    chore: change flyway init to work on native mode

--- vertx/src/main/java/org/acme/config/RunFlyway.java
+++ vertx/src/main/java/org/acme/config/RunFlyway.java
@@ -1,10 +1,18 @@
 package org.acme.config;
 
+import io.quarkus.arc.Arc;
+import io.quarkus.arc.InstanceHandle;
+import io.quarkus.flyway.runtime.FlywayContainer;
+import io.quarkus.flyway.runtime.FlywayContainerProducer;
+import io.quarkus.flyway.runtime.QuarkusPathLocationScanner;
 import io.quarkus.runtime.StartupEvent;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.event.Observes;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.flywaydb.core.Flyway;
+
+import javax.sql.DataSource;
+import java.util.List;
 
 @ApplicationScoped
 public class RunFlyway {
@@ -16,8 +24,16 @@ public class RunFlyway {
     @ConfigProperty(name = "quarkus.datasource.password")
     String datasourcePassword;
 
+    @ConfigProperty(name = "flyway.native.files")
+    List<String> files;
+
     public void runFlywayMigration(@Observes StartupEvent event) {
-        Flyway flyway = Flyway.configure().dataSource("jdbc:" + datasourceUrl, datasourceUsername, datasourcePassword).load();
-        flyway.migrate();
+        QuarkusPathLocationScanner.setApplicationMigrationFiles(files);
+        DataSource ds = Flyway.configure().dataSource("jdbc:" + datasourceUrl, datasourceUsername, datasourcePassword).getDataSource();
+        try (InstanceHandle<FlywayContainerProducer> instanceHandle = Arc.container().instance(FlywayContainerProducer.class)) {
+            FlywayContainer flywayContainer = instanceHandle.get().createFlyway(ds, "<default>", true, true);
+            Flyway flyway = flywayContainer.getFlyway();
+            flyway.migrate();
+        }
     }
 }
