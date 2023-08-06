e66efb6e3c759bb7d77069ba9c23c04f8bbbdce4
andrepaschoal <paschoal.andre@gmail.com>
2023-08-06 21:19:01 +0200

    [spring-boot] Dummy PeopleController

--- spring-boot/src/main/java/com/people/peoplemanager/api/PeopleController.java
+++ spring-boot/src/main/java/com/people/peoplemanager/api/PeopleController.java
@@ -2,15 +2,30 @@ package com.people.peoplemanager.api;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.List;
 
 @RestController
 @RequestMapping("/pessoas")
 public class PeopleController {
     @PostMapping
     public ResponseEntity add() {
-        return new ResponseEntity<>(HttpStatus.CREATED);
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public List<String> search() {
+        return Collections.emptyList();
+    }
+
+    @GetMapping("/{id}")
+    public String get(@PathVariable String id) {
+        return "Hi, I'm person " + id;
     }
 }
