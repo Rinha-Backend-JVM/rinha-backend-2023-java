f7964e324c42d1b32e1ba3e5da7c39fb6e7dc34e
andrepaschoal <paschoal.andre@gmail.com>
2023-08-06 21:18:01 +0200

    Revert "sdadasda"
    This reverts commit d4d9c0a65ef744307ca5a64be6e6a513bcb34376.

--- spring-boot/src/main/java/com/people/peoplemanager/api/PeopleController.java
+++ spring-boot/src/main/java/com/people/peoplemanager/api/PeopleController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/pessoas")
 public class PeopleController {
-    private String abc;
     @PostMapping
     public ResponseEntity add() {
         return new ResponseEntity<>(HttpStatus.CREATED);
