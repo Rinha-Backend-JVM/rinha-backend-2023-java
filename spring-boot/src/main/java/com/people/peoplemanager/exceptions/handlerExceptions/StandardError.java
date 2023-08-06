a9a033f35b1593f67ceb04b231dd3fd2dd5ab0ef
Bruno Santos <bruno4t@hotmail.com>
2023-08-06 12:21:26 -0300

    ALteração de class para record

--- spring-boot/src/main/java/com/people/peoplemanager/exceptions/handlerExceptions/StandardError.java
+++ spring-boot/src/main/java/com/people/peoplemanager/exceptions/handlerExceptions/StandardError.java
@@ -2,62 +2,9 @@ package com.people.peoplemanager.exceptions.handlerExceptions;
 
 import java.time.Instant;
 
-public class StandardError {
-
-    private Instant timeStamp;
-    private Integer status;
-    private String error;
-    private String message;
-    private String path;
-
-    public StandardError() {
-    }
-
-    public StandardError(Instant timeStamp, Integer status, String error, String message, String path) {
-        this.timeStamp = timeStamp;
-        this.status = status;
-        this.error = error;
-        this.message = message;
-        this.path = path;
-    }
-
-    public Instant getTimeStamp() {
-        return timeStamp;
-    }
-
-    public void setTimeStamp(Instant timeStamp) {
-        this.timeStamp = timeStamp;
-    }
-
-    public Integer getStatus() {
-        return status;
-    }
-
-    public void setStatus(Integer status) {
-        this.status = status;
-    }
-
-    public String getError() {
-        return error;
-    }
-
-    public void setError(String error) {
-        this.error = error;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public String getPath() {
-        return path;
-    }
-
-    public void setPath(String path) {
-        this.path = path;
-    }
-}
+public record StandardError(
+        Instant timeStamp,
+        Integer status,
+        String error,
+        String message,
+        String path     ) {}
