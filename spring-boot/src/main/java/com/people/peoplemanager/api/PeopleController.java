480883c27d45fec3c9e842cde82a2b1fc50d8ad6
andrepaschoal <paschoal.andre@gmail.com>
2023-08-06 21:57:00 +0200

    [spring-boot] Add Payload and Location header to dummy PeopleController POST

--- spring-boot/src/main/java/com/people/peoplemanager/api/PeopleController.java
+++ spring-boot/src/main/java/com/people/peoplemanager/api/PeopleController.java
@@ -5,9 +5,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URI;
+import java.time.Instant;
 import java.util.Collections;
 import java.util.List;
 
@@ -15,8 +18,12 @@ import java.util.List;
 @RequestMapping("/pessoas")
 public class PeopleController {
     @PostMapping
-    public ResponseEntity add() {
-        return new ResponseEntity(HttpStatus.CREATED);
+    public ResponseEntity add(
+            @RequestBody PersonDto personDto
+    ) {
+        return ResponseEntity.created(
+                URI.create("/pessoas/" + Instant.now().toEpochMilli()) // some dummy created resource Location
+        ).build();
     }
 
     @GetMapping
@@ -29,3 +36,5 @@ public class PeopleController {
         return "Hi, I'm person " + id;
     }
 }
+
+record PersonDto(String nome, String apelido, String nascimento, String[] stack) {}
